0622ef397303e27e5ccc708d36cb7a0d90724675
windowcow <65646883+windowcow@users.noreply.github.com>
2022-12-10 13:39:36 +0900

    making regex

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ $(document).ready(function () {
     $('#add-choice-btn').on('click', function (event) {
         addChoiceBtnClicked(event)
     });
-
+    $('#question').on('input', function (event) {
+        questionChanged(event)
+    });
 });
 
 function addChoiceBtnClicked(event) {
     event.preventDefault();
     console.log('addChoiceBtnClicked');
 };
+
+
+
+function questionChanged(event) {
+    var rightChoicePattern = /-\[[oO]]\s(.*)\n/;
+    var wrongChoicePattern = /-\[[xX]]\s(.*)\n/;
+    console.log('questionChanged');
+}
